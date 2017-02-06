e91e88a33ed9d210f59c29d085a905c3c8881070
Michael Olenchuk <Michael Olenchuk>
2017-02-06 16:08:30 -0500

    Modifyed after testing

--- database/User.sql
+++ database/User.sql
@@ -1,8 +1,8 @@
 CREATE TABLE user(						//Creation of user table
 	f_name VARCHAR(30) NOT NULL,				//First name row
 	l_name VARCHAR(30) NOT NULL,				//Last name row
-	email VARCHAR(60) NOT NULL,				//email row
-	username VARCHAR(30) NOT NULL,				//username to match with during log in
-	date_entered TIMESTAMP,					//date user entered UpGrade
+	email VARCHAR(60) NOT NULL UNIQUE,				//email row
+	username VARCHAR(30) NOT NULL UNIQUE,				//username to match with during log in
+	date_entered TIMESTAMP DEFAULT CURRENT_TIMESTAMP,					//date user entered UpGrade
 	u_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,		//used to keep track of each user
 	password VARCHAR(20) NOT NULL);						//for password
