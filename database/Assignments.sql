2930b8596c4a3f395057a1fbc158ced7e5c43b94
Michael Olenchuk <Michael Olenchuk>
2017-02-13 18:57:23 -0500

    removed grade_given, changed grade_weight, changed a_desc

--- database/Assignments.sql
+++ database/Assignments.sql
@@ -1,10 +1,9 @@
 CREATE TABLE Assignments(							//table for assignments and grades
 	a_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,	//id for each assaignment
 	p_id INT,										//possible id to link assigments to other assaignments
-	c_id INT NOT NULL,								//forigen key to classes
-	grade_given DOUBLE,								//grade given by teacher
-	grade_weight INT NOT NULL,						//weight of the assigment in the course
-	a_desc VARCHAR(100),							//description of the assaignment
+	c_id INT NOT NULL,								//forigen key to classes							
+	grade_weight INT,						//weight of the assigment in the course
+	a_desc VARCHAR(500),							//description of the assaignment
 	date_assigned DATE,								//date assigment is assigned
 	date_due DATE,									//date assigment is due
 	a_name VARCHAR(30) NOT NULL,					//name of assignment
@@ -15,3 +14,6 @@ CREATE TABLE Assignments(							//table for assignments and grades
 		REFERENCES Course(c_id)
 		ON DELETE CASCADE
 		ON UPDATE CASCADE);
+		
+	
+	//changes on 2/13: took out grade_given and made a new table for it, changed grade_weight allowing null, changed description to 500
