a1ad2669a0995d54ee8d3f139387e53a16ab3e40
BenGirone <BGirone001@student.franciscan.edu>
2017-02-22 14:12:52 -0500

    added bash scripts /shell to send emails

---
+++ shell/registrationEmail.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+RECIPIENT=$1
+USER=$2
+CODE=$3
+CONTAINER="/var/www/html/351tests/shell/container"
+
+echo "To: $RECIPIENT" >> $CONTAINER
+echo "From: NoReply.upGrade.App@gmail.com" >> $CONTAINER
+echo "Subject: upGrade - Registration Confirmation Code" >> $CONTAINER
+echo "" >> $CONTAINER
+echo "Hello $USER, Please enter the following numerical code to complete your registration at upGrade" >> $CONTAINER
+echo "" >> $CONTAINER
+echo "Your Code is: $CODE" >> $CONTAINER
+/usr/sbin/ssmtp $RECIPIENT < $CONTAINER
+
+> $CONTAINER
+
