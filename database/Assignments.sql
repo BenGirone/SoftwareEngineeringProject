92812d07989098c137f52019172f5757fbe72554
BenGirone <BGirone001@student.franciscan.edu>
2017-02-22 14:05:07 -0500

    fixed michael's comment notation

--- database/Assignments.sql
+++ database/Assignments.sql
@@ -1,19 +1,19 @@
-CREATE TABLE Assignments(							//table for assignments and grades
-	a_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,	//id for each assaignment
-	p_id INT,										//possible id to link assigments to other assaignments
-	c_id INT NOT NULL,								//forigen key to classes							
-	grade_weight INT,						//weight of the assigment in the course
-	a_desc VARCHAR(500),							//description of the assaignment
-	date_assigned DATE,								//date assigment is assigned
-	date_due DATE,									//date assigment is due
-	a_name VARCHAR(30) NOT NULL,					//name of assignment
+CREATE TABLE Assignments(							/*table for assignments and grades*/
+	a_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,	/*id for each assaignment*/
+	p_id INT,										/*possible id to link assigments to other assaignments*/
+	c_id INT NOT NULL,								/*forigen key to classes*/
+	grade_weight INT,						/*weight of the assigment in the course*/
+	a_desc VARCHAR(500),							/*description of the assaignment*/
+	date_assigned DATE,								/*date assigment is assigned*/
+	date_due DATE,									/*date assigment is due*/
+	a_name VARCHAR(30) NOT NULL,					/*name of assignment*/
 	
 	
-	INDEX (c_id),					//added on to try to create relationships
+	INDEX (c_id),					/*added on to try to create relationships*/
 	FOREIGN KEY (c_id)
 		REFERENCES Course(c_id)
 		ON DELETE CASCADE
 		ON UPDATE CASCADE);
 		
 	
-	//changes on 2/13: took out grade_given and made a new table for it, changed grade_weight allowing null, changed description to 500
+	/*changes on 2/13: took out grade_given and made a new table for it, changed grade_weight allowing null, changed description to 500
