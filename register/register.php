28566ea3e69dcbff379af295d7da3d4d713dbcf1
BenGirone <BGirone001@student.franciscan.edu>
2017-03-02 15:17:14 -0500

    testing registration code bug 2

--- register/register.php
+++ register/register.php
@@ -42,7 +42,7 @@ if(isset($_POST["username"]) && isset($_POST["password"]) && isset($_POST["email
 		//$letters = array('a', 'b', 'c', 'd', 'e' 'f');
 		//$code = rand(10000, 99999) . $letters[rand(0,5)];
 		$code = '1';
-		exec("cd ../shell && ./registrationEmail.sh '$email' '$username' '$code'");
+		exec("cd .. && cd shell && ./registrationEmail.sh '$email' '$username' '$code'");
 		echo "success";
 	}
 }
