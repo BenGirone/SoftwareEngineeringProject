a0c7e3d908f4c1cf77f6c5ad776d871289f7480e
BenGirone <BGirone001@student.franciscan.edu>
2017-04-05 01:17:46 -0400

    made executable into a linux output

--- test/index.php
+++ test/index.php
@@ -1,8 +1,4 @@
 <?php
     $derpVar = "0.89_100_0.85_100_-1_100_-1_100_0.90_";
-
-    chdir("../C");
-    exec("algorithm.exe $derpVar", $out);
-    echo $out[0];
-    chdir("../test");
+    echo shell_exec("cd .. && cd C && ./binary $derpVar");
 ?>
