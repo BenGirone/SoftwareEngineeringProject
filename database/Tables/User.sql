7df89c20ea384b9525cae649b90272d5ecfe9235
Michael Olenchuk <Michael Olenchuk>
2017-04-18 15:55:44 -0400

    Fixed password size

--- database/Tables/User.sql
+++ database/Tables/User.sql
@@ -3,6 +3,6 @@ CREATE TABLE user(										/*Creation of user table*/
 	username VARCHAR(30) NOT NULL UNIQUE,				/*username to match with during log in Alternate Key*/
 	date_entered TIMESTAMP DEFAULT CURRENT_TIMESTAMP,	/*date user entered upgrade*/
 	u_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,		/*used to keep track of each user*/
-	password VARCHAR(20) NOT NULL,						/*keep password for future reference*/
+	password VARCHAR(255) NOT NULL,						/*keep password for future reference*/
 	registrationCode VARCHAR(60) NOT NULL,				/*sends code to email*/
 	is_registered BIT DEFAULT 0);						/*helps make sure a user is conected through email*/
